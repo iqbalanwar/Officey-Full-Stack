86b052ea93b347e5a26ce3f6172405cb406f99a1
Iqbal Anwar <iqbalaanwar@gmail.com>
2019-10-04 15:21:43 -0400

    Initial commit, spring boot project and front-end

---
+++ officey/src/main/java/com/example/officey/OfficeyApplication.java
@@ -0,0 +1,13 @@
+package com.example.officey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeyApplication.class, args);
+	}
+
+}
