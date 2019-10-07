156a9e14a8f8fd69f2074d60e5b5496a72d61448
YOUR NAME HERE <ULLOA_SANTIAGO@YAHOO.COM>
2019-10-07 14:29:24 -0400

    Created User Profile

--- officey/src/main/java/com/example/officey/repository/UserProfileRepository.java
+++ officey/src/main/java/com/example/officey/repository/UserProfileRepository.java
@@ -1,16 +1,16 @@
-//package com.example.officey.repository;
-//
-//import com.example.officey.model.UserProfile;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface UserProfileRepository extends CrudRepository<UserProfile, Long> {
-//
-////    @Query("from UserProfile up left join User u on u.username = ?1 and up.id = u.userProfile.id")
-////    public UserProfile findProfileByUsername(String username);
-//
-//
-//    @Query("from UserProfile up inner join User u on u.username = ?1 and up.id = u.userProfile.id")
+package com.example.officey.repository;
+
+import com.example.officey.model.UserProfile;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserProfileRepository extends CrudRepository<UserProfile, Long> {
+
+//    @Query("from UserProfile up left join User u on u.username = ?1 and up.id = u.userProfile.id")
 //    public UserProfile findProfileByUsername(String username);
-//
-//}
+
+
+    @Query("from UserProfile up inner join User u on u.username = ?1 and up.id = u.userProfile.id")
+    public UserProfile findProfileByUsername(String username);
+
+}
