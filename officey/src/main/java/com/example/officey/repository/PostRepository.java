14729a33e4631ce0d26732130628c85d139b4d83
Iqbal Anwar <iqbalaanwar@gmail.com>
2019-10-07 12:28:01 -0400

    User can make and delete posts

--- officey/src/main/java/com/example/officey/repository/PostRepository.java
+++ officey/src/main/java/com/example/officey/repository/PostRepository.java
@@ -1,4 +1,9 @@
 package com.example.officey.repository;
 
-public interface PostRepository {
+import com.example.officey.model.Post;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends CrudRepository<Post, Long> {
 }
