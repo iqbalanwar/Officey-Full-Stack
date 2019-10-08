aec8809e5b492ed9d2c254924b47a81181a407bd
YOUR NAME HERE <ULLOA_SANTIAGO@YAHOO.COM>
2019-10-08 13:38:02 -0400

    Completed UserControllerTest

--- officey/src/test/java/com/example/officey/controller/UserControllerTest.java
+++ officey/src/test/java/com/example/officey/controller/UserControllerTest.java
@@ -1,4 +1,99 @@
 package com.example.officey.controller;
 
+import com.example.officey.controller.UserController;
+import com.example.officey.config.JwtUtil;
+import com.example.officey.service.UserService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeEditor;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.junit.runner.RunWith;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
+
+    /**Main entry point for server-side Spring MVC test support.**/
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private JwtUtil jwtUtil;
+
+    @Test
+    public void helloWorld_ReturnsString_Success() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/hello")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World!!"));
+    }
+
+    @Test
+    public void login_Success() throws Exception{
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createUserInJson("joe","abc"));
+
+        when(userService.login(any())).thenReturn("123456");
+
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"token\":\"123456\"}"))
+                .andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    @Test
+    public void signUp_Success() throws Exception{
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createUserInJson("joe","abc"));
+
+        when(userService.createUser(any())).thenReturn("123456");
+
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"token\":\"123456\"}"))
+                .andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    private static String createUserInJson (String name, String password) {
+        return "{ \"username\": \"" + name + "\", " +
+                "\"password\":\"" + password+"\"}";
+    }
+
+//    @Test
+//    public void listUsers_Success() throws Exception{
+//        RequestBuilder requestBuilder = MockMvcRequestBuilders
+//                .get("/user/list")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(createUserInJson("joe", "abc"));
+//    }
+
 }
