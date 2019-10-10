ee5750b555fdb3fefa0bcfb1dfd07bbc18eaece9
Iqbal Anwar <iqbalaanwar@gmail.com>
2019-10-10 09:12:33 -0400

    List of all posts can be shown in the front-end

--- officey/src/main/java/com/example/officey/config/SecurityConfig.java
+++ officey/src/main/java/com/example/officey/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -37,6 +38,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
 
         http.csrf().disable()
                 .authorizeRequests()
